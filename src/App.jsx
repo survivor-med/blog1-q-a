5e4de6d19fcfdf1200c410a6adca0ed16cf6b377
survivor-med <ontightrope@gmail.com>
2025-09-10 12:19:21 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">블로그 Q&A 챗봇</h1>
+      <p>성공적으로 배포됐습니다 🎉</p>
+    </div>
+  )
+}
