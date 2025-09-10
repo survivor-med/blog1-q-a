058c24733574a08f59af7973c35cb8b871953e22
survivor-med <ontightrope@gmail.com>
2025-09-10 13:18:28 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import './index.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
